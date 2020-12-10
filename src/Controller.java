8bf10ed0f0ff4d77a01b9495e71991c1f4b984f2
Nghia <buitrungnghia1.11@gmail.com>
2020-12-10 23:29:22 +0700

    demo

---
+++ src/Controller.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    List<Product> products;
+    public Controller(){
+        products = new ArrayList<>();
+        Product product1 = new Product();
+        Category category = new Category();
+        category.setName("demo cate");
+        product1.setId(1);
+        product1.setCategory(category);
+    }
+    public void showCategory(){
+        for (Product product : products){
+            if (product.getCategory().getName().equals("demo cate")) {
+                System.out.println("chac la no nhu the nay day");
+            }
+        }
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+    private int id = 1;
+    private Category category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
